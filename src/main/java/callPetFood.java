f660d2bcf542d7eb3573b9d0b78b22cfe429634c
VanDiBandit <brovan45@gmail.com>
2022-09-19 17:30:16 -0400

    Array pet hw

---
+++ src/main/java/callPetFood.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+public class callPetFood {
+
+    public static void main(String[] args) {
+
+arrayPetFood[] dog ={ new arrayPetFood("Doggo","Biscuits")};
+arrayPetFood[] cat={new arrayPetFood("Meowsie", "Salmon")};
+arrayPetFood [] pig={new arrayPetFood("Oinkers","Slop")};
+arrayPetFood[] mouse={new arrayPetFood("Jerry","Cheese")};
+
+System.out.println(Arrays.toString(dog));
+        System.out.println(Arrays.toString(cat));
+        System.out.println(Arrays.toString(pig));
+        System.out.println(Arrays.toString(mouse));
+
+
+    }
+}
