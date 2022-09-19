25d392a7d392fab89e2c25d9f18e4820a2643a91
VanDiBandit <brovan45@gmail.com>
2022-09-19 17:30:16 -0400

    Array pet hw

---
+++ src/main/java/arrayHw/arrayPetFood.java
@@ -0,0 +1,25 @@
+package arrayHw;
+
+public class arrayPetFood {
+    private String petName;
+    private String petFood;
+    //constructor for calling pets name & food
+    public arrayPetFood(String petName,String petFood) {
+        this.petName = petName;
+        this.petFood = petFood;
+    }
+    public String getPetName(){
+        return petName;
+    }
+    public String getPetFood(){
+        return petFood;
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+                "My pet's name is a, " + petName + '\''+
+                ", & it eat's " + petFood + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/arrayHw/callPetFood.java
@@ -0,0 +1,23 @@
+package arrayHw;
+
+import arrayHw.arrayPetFood;
+
+import java.util.Arrays;
+
+public class callPetFood {
+
+    public static void main(String[] args) {
+
+arrayPetFood[] dog ={ new arrayPetFood("Doggo","Biscuits")};
+arrayPetFood[] cat={new arrayPetFood("Meowsie", "Salmon")};
+arrayPetFood [] pig={new arrayPetFood("Oinkers","Slop")};
+arrayPetFood[] mouse={new arrayPetFood("Jerry","Cheese")};
+
+System.out.println(Arrays.toString(dog));
+        System.out.println(Arrays.toString(cat));
+        System.out.println(Arrays.toString(pig));
+        System.out.println(Arrays.toString(mouse));
+
+
+    }
+}
